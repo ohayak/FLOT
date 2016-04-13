ef12a0d895554975babdc8ae852ae0c188c9bd51
ohayak <omar.hayak@enseirb.fr>
2016-04-13 18:11:29 +0200

    first commit

---
+++ src/InsertHeuristicTSP.java
@@ -0,0 +1,19 @@
+package tsp;
+import java.util.List;
+
+/**
+ * 
+ * This heuristic iteratively appends a customer
+ * to the current solution until a tour is obtained
+ *
+ */
+
+public class InsertHeuristicTSP implements HeuristicTSP {
+
+	/** TODO : coder cette méthode */
+	public double computeSolution(double[][] matrix, List<Integer> solution) {
+		double value = 0.0;
+		return value;
+	}
+
+}
